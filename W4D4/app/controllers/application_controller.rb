ce59c51a833d77d6338f1b5902de7d380b791e86
jiangtt18 <tjiang90111@gmail.com>
2018-03-08 22:49:09 -0800

    W4D5

--- W4D4/app/controllers/application_controller.rb
+++ W4D4/app/controllers/application_controller.rb
@@ -16,16 +16,20 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    !current_user.nil?
+    # !current_user.nil?
+    !!current_user
   end
 
   def login_user!(user)
     session[:session_token] = user.reset_session_token!
+#everytime user log in . will be assigned a new token
   end
 
   def logout_user!
     current_user.reset_session_token!
+
     session[:session_token] = nil
+
   end
 
   def require_user!
