705fda3e7e165dd24c9b02e4fab85dc0bdbe4c03
jiangtt18 <tjiang90111@gmail.com>
2018-03-28 00:59:46 -0700

    W7D3'

---
+++ W7D3/components/giphys_search.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {searchTerm: "golden retriever"};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  componentDidMount() {
+  this.props.fetchSearchGiphys('golden+retriver');
+  }
+
+  handleChange(e){
+  this.setState({searchTerm: e.currentTarget.value});
+  }
+
+  handleSubmit(e){
+  e.preventDefault();
+  let searchTerm = this.state.searchTerm.split(' ').join('+');
+  this.prop.fetchSearchGiphys(searchTerm);
+  }
+
+  render(){
+  let {giphys} = this.props;
+  return (
+    <div>
+      <form>
+        <input value = {this.state.searchTerm} onChange= {this.handleChange}/>
+        <button type= 'submit' onClick= {this.handleSubmit}>Search</button>
+      </form>
+      <GiphysIndex giphys = {giphys}/>;
+    </div>
+  );
+  }
+}
+
+export default GiphysSearch;
---
+++ W7D3/reducers/giphys_reducer.js
@@ -0,0 +1,11 @@
+import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
+
+const giphysReducer = (oldState=[],action) =>{
+  switch(action.type) {
+    case RECEIVE_SEARCH_GIPHYS:
+      return action.giphys;
+    default:
+      return oldState;
+  }
+};
+export default giphysReducer;
